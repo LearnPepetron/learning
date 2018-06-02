852eec6eee8940dab29b3dd7ada64ae2790cb16c
pepetronify <39862156+pepetronify@users.noreply.github.com>
2018-06-02 20:29:48 +0200

    Initial commit

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package HolaMundo;
+
+/**
+ * Created by mongoDB on 27/05/2018.
+ */
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo JL comienza con java");
+    }
+
+}
